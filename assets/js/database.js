3aa8435a0d4f020722799663f574381005b1f65e
Flepix <matepz10@gmail.com>
2023-06-16 01:04:13 -0300

    Base de la pagina

---
+++ assets/js/database.js
@@ -0,0 +1,77 @@
+const items = [
+    //Assassin Items
+    {
+        "id": 1,
+        "name": "",
+        "class": "Assassin",
+        "info": "",
+        "img": ""
+
+    },
+    {
+        "id": 2,
+        "name": "",
+        "class": "Assassin",
+        "info": "",
+        "img": ""
+
+    },
+    //Fighter Items
+    {
+        "id": ,
+        "name": "",
+        "class": "Fighter",
+        "info": "",
+        "img": ""
+
+    },
+    //Jungle Items
+    {
+        "id": ,
+        "name": "",
+        "class": "Jungle",
+        "info": "",
+        "img": ""
+
+    },
+    //Mage Items
+    {
+        "id": ,
+        "name": "",
+        "class": "Mage",
+        "info": "",
+        "img": ""
+
+    },
+    //Marksman Items
+    {
+        "id": ,
+        "name": "",
+        "class": "Marksman",
+        "info": "",
+        "img": ""
+
+    },
+    //Support Items
+    {
+        "id": ,
+        "name": "",
+        "class": "Support",
+        "info": "",
+        "img": ""
+
+    },
+    //Tank Items
+    {
+        "id": ,
+        "name": "",
+        "class": "Tank",
+        "info": "",
+        "img": ""
+
+    },
+    {
+
+    }
+
+];
