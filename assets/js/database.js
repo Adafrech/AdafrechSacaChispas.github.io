791c9c453687a83186166a95997fd5997cffa15a
Ripper3sh <17romerojulian@gmail.com>
2023-06-20 00:32:27 -0300

    Items de Support en la base de datos.

--- assets/js/database.js
+++ assets/js/database.js
@@ -18,7 +18,7 @@ const items = [
     },
     //Fighter Items
     {
-        "id": ,
+        "id": 3,
         "name": "",
         "class": "Fighter",
         "info": "",
@@ -27,7 +27,7 @@ const items = [
     },
     //Jungle Items
     {
-        "id": ,
+        "id": 4,
         "name": "",
         "class": "Jungle",
         "info": "",
@@ -36,7 +36,7 @@ const items = [
     },
     //Mage Items
     {
-        "id": ,
+        "id": 5,
         "name": "",
         "class": "Mage",
         "info": "",
@@ -45,7 +45,7 @@ const items = [
     },
     //Marksman Items
     {
-        "id": ,
+        "id": 6,
         "name": "",
         "class": "Marksman",
         "info": "",
@@ -54,16 +54,48 @@ const items = [
     },
     //Support Items
     {
-        "id": ,
-        "name": "",
+        "id": 7,
+        "name": "Canto de batalla de Shurelya",
         "class": "Support",
-        "info": "",
-        "img": ""
+        "info": "Activa: ÚNICA: te otorga a ti y a todos tus aliados en un radio de 1000 unidades un 30 % de velocidad de movimiento adicional durante 4 segundos ( 75 segundos de enfriamiento).",
+        "img": "https://i.postimg.cc/RVcbNQj1/shurelyas-battlesong.jpg"
+
+    },
+    {
+        "id": 8,
+        "name": "Renovador de Piedra Lunar",
+        "class": "Support",
+        "info": "Pasiva ÚNICA: Curar o proteger a un campeón aliado encadena el efecto al otro campeón aliado más cercano dentro de las 800 unidades de ellos (excluyéndote a ti), otorgándoles el 35 % de la curación o el 40 % del escudo. Si no hay otros campeones aliados en el radio, otorga al mismo objetivo un 25 % adicional de curación o un 30 % de escudo.",
+        "img": "https://i.postimg.cc/J7cRWJPg/moonstone-renewer.png"
+
+    },
+    {
+        "id": 9,
+        "name": "Ecos de Helia",
+        "class": "Support",
+        "info": "Pasiva ÚNICA: Dañar a un campeón enemigo con un ataque básico o daño de habilidad otorga un fragmento de alma, hasta 2 . Curar o proteger a un campeón aliado (excluyéndote a ti) consume todos los fragmentos de alma para curarlos en 20 − 80 por fragmento y causar 30 − 180 de daño mágico por fragmento al campeón enemigo más cercano (según el nivel del aliado).",
+        "img": "https://i.postimg.cc/q7PQQNKX/echoes-of-helia.jpg"
+
+    },
+    {
+        "id": 10,
+        "name": "Sudario Uniforme",
+        "class": "Support",
+        "info": "Pasiva ÚNICA: ser afectado por un campeón enemigo o aplicar un efecto de inmovilización o aterramiento a él y a todos los campeones enemigos dentro de 600 unidades de ti con Arrepentimiento, lo que aumenta el daño que reciben en un 10 % durante 5 segundos.",
+        "img": "https://i.postimg.cc/NGmGdS31/evenshroud.jpg"
+
+    },
+    {
+        "id": 11,
+        "name": "Guardapelo del Solari de Hierro",
+        "class": "Support",
+        "info": "Activo ÚNICO: Te otorga a ti y a tus campeones aliados dentro de 850 unidades un escudo por 200 − 360 (dependiendo del nivel del objetivo) que decae en 2 . 5 segundos ( 90 segundos de enfriamiento).",
+        "img": "https://i.postimg.cc/MTz5ghsv/locket-of-the-iron-solari.jpg"
 
     },
     //Tank Items
     {
-        "id": ,
+        "id": 999,
         "name": "",
         "class": "Tank",
         "info": "",
